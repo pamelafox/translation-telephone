aa3adca09d4f49d34159c5e63cdc75a30af2bcfe
Pamela Fox <pamela.fox@gmail.com>
2022-07-07 15:25:04 -0700

    Fix os.get

--- src/translations.py
+++ src/translations.py
@@ -10,7 +10,7 @@ def translate_with_azure(text, from_lang, to_lang):
     constructed_url = endpoint + path
     params = {"api-version": "3.0", "from": from_lang, "to": to_lang}
     headers = {
-        "Ocp-Apim-Subscription-Key": os.environ["AZURE_TRANSLATE_API_KEY"],
+        "Ocp-Apim-Subscription-Key": os.environ.get("AZURE_TRANSLATE_API_KEY", "NoKeyFound"),
         "Ocp-Apim-Subscription-Region": location,
         "Content-type": "application/json",
         "X-ClientTraceId": str(uuid.uuid4()),
